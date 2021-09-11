a6e6351ca8ceffe13bd3dfb42372f870b1fe72b7
Раиль Хайруллин <rail.khayrullin@gmail.com>
2021-09-11 12:34:38 +0500

    fix bags in command manager

--- apps/blog/management/commands/initadmin.py
+++ apps/blog/management/commands/initadmin.py
@@ -5,7 +5,10 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     """Команда для создания тестового суперпользователя"""
     def handle(self, *args, **options):
-        User.objects.create_superuser(username="test_admin", email="test_admin@gmail.com", password="test_admin")
-        print('Test admin account create with:\n'
-              'username="test_admin"\n'
-              'password="test_admin"')
+        if User.objects.get(username="test_admin"):
+            print('This admin account exist.')
+        else:
+            User.objects.create_superuser(username="test_admin", email="test_admin@gmail.com", password="test_admin")
+            print('Test admin account create with:\n'
+                  'username="test_admin"\n'
+                  'password="test_admin"')
